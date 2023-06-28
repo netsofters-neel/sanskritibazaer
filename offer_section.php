13fcb8009308f24df9101831ad730ce8bebfd2e2
netsofters-neel <126561179+netsofters-neel@users.noreply.github.com>
2023-06-28 17:44:34 +0530

    Update offer_section.php

--- offer_section.php
+++ offer_section.php
@@ -1,1 +1,25 @@
-
+ <?php if (has_permissions('read', 'featured_section')) { ?>
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link menu-open">
+                            <i class="nav-icon fas fa-layer-group text-danger"></i>
+                            <p>
+                                Featured Sections
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="<?= base_url('admin/featured-sections/') ?>" class="nav-link">
+                                    <i class="fas fa-folder-plus nav-icon"></i>
+                                    <p>Manage Sections</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="<?= base_url('admin/featured-sections/section-order') ?>" class="nav-link">
+                                    <i class="fa fa-bars nav-icon"></i>
+                                    <p>Sections Order</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php } ?>
